e8c9e48d29e6656f3ede1697f72caa3387c49689
kryptx <kryptx@gmail.com>
2013-07-02 19:19:31 -0600

    Changed category to dropdown

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,7 +5,4 @@
 angular.module('fablr.controllers', ['fablr.services']).
 	controller('Story', ['$scope', 'Categories', function($scope, Categories) {
 		$scope.categories = Categories;
-		$scope.title = '';
-		$scope.category = '';
-		$scope.firstPage = '';
 	}]);
